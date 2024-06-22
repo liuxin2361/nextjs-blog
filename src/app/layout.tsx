7fcc62398b53f2d0fd5778bd75782339423e0628
Xin Liu <37545515+liuxin2361@users.noreply.github.com>
2024-06-22 19:25:23 +0900

    fix bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           {children}
         </ThemeProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
